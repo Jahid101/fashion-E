dd78127c430922673f149ba1a572ea1d7ee1d1bb
Robustdevs5 <jahidhasananik.official@gmail.com>
2022-09-27 00:18:55 +0600

    topbar

--- Components/MobileDrawer/index.tsx
+++ Components/MobileDrawer/index.tsx
@@ -1,119 +1,147 @@
-import { Avatar, Drawer, Menu,Layout } from 'antd';
-import Link from 'next/link';
-import router from 'next/router';
+import { Avatar, Drawer, Menu, Layout, message } from "antd";
+import Link from "next/link";
+import router from "next/router";
 import { FaUserCircle } from "react-icons/fa";
 import { MdDashboard, MdShoppingBag } from "react-icons/md";
 import { GiMagnifyingGlass } from "react-icons/gi";
 import { RiFileCopy2Line, RiFileTextLine } from "react-icons/ri";
 import { IoMdPie } from "react-icons/io";
 import { BsFillFileEarmarkTextFill } from "react-icons/bs";
-import { GiGooeyEyedSun } from "react-icons/gi";
-import React from 'react';
-require('./index.less');
-
+import { GrFacebook } from "react-icons/gr";
+import { BsInstagram } from "react-icons/bs";
+import { FaPinterestSquare } from "react-icons/fa";
+import {
+  UserOutlined,
+  ShoppingCartOutlined,
+  HomeOutlined,
+  ProfileOutlined,
+  SnippetsOutlined,
+  IssuesCloseOutlined,
+} from "@ant-design/icons";
+import React from "react";
+require("./index.less");
 
 const { SubMenu } = Menu;
 const { Sider } = Layout;
 
-
 const index = (props: any) => {
-    const { SubMenu } = Menu;
+  const { SubMenu } = Menu;
 
-
-    return (
+  return (
+    <div>
+      <Drawer
+        title={
+          <Link href="/">
+            <a href="/">
+              <div
+                className="navbarTitle primary-color"
+                style={{ marginLeft: "20px" }}
+              >
+                Funshion
+              </div>
+            </a>
+          </Link>
+        }
+        placement="left"
+        width={"100%"}
+        className="navMobile"
+        closable={true}
+        onClose={() => {
+          props.setVisible(false);
+        }}
+        visible={props.visible}
+      >
         <div>
-            <Drawer
-                title={
-                    <img
-                        onClick={() => {
-                            router.push("/")
-                        }}
-                        className="cursor"
-                        alt="Logo" src="/images/siteLogo.svg"></img>
-                }
-                placement="left"
-                width={"100%"}
-                className="navMobile"
-                closable={true}
-                onClose={() => { props.setVisible(false) }}
-                visible={props.visible}
+          <div className="flex">
+            <div>
+              <Avatar
+                className="mr-10 ml-15 cursor"
+                style={{ border: "1px solid #c9c1c1" }}
+                size={85}
+                //@ts-ignore
+                icon={<img src="/images/profileBig.svg" alt="" />}
+                shape="circle"
+              />
+            </div>
+
+            <div className="ml-15 m-auto">
+              <div className="wordBreak primary-color text-18">{"Mr. XYZ"}</div>
+              <div className="wordBreak white-color">{"xyz@gmail.com"}</div>
+            </div>
+          </div>
+
+          <div className="mt-50">
+            <Menu
+              theme="light"
+              mode="inline"
+              defaultOpenKeys={[props?.defaultOpenKeys]}
+              selectedKeys={[props?.defaultSelectedKeys]}
             >
-                <div>
-                    <div className="flex">
-                        <div>
-                            <Avatar
-                                className="mr-10 ml-15 cursor"
-                                style={{ width: 85, height: 85, border: "1px solid #c9c1c1" }}
-                                //@ts-ignore
-                                icon={
-                                    <img
-                                        src={"/images/profileBig.svg"}
-                                        onError={(e: any) => { }}
-                                        alt=""
-                                    />
-                                }
-                                shape="circle"
-                            />
-                        </div>
+              <Menu.Item key="1" icon={<HomeOutlined />} className="mb-20">
+                <Link href="/">Home</Link>
+              </Menu.Item>
 
-                        <div className="ml-15 m-auto">
-                            <div className="wordBreak primary-color text-18">{"Alexa Jui"}</div>
-                            <div className="wordBreak white-color">{"alexajui@gmail.com"}</div>
-                        </div>
-                    </div>
+              <Menu.Item
+                key="2"
+                icon={<IssuesCloseOutlined />}
+                className="mb-20"
+              >
+                <Link href="/about">About</Link>
+              </Menu.Item>
 
-                    <div className="mt-50">
-                        
-                        <Menu
-                            theme="light"
-                            mode="inline"
-                        defaultOpenKeys={[props?.defaultOpenKeys]}
-                        selectedKeys={[props?.defaultSelectedKeys]}
-                        >
-                            <Menu.Item key="1" icon={<MdDashboard />} className="mb-20">
-                                <Link href="/">Dashboard</Link>
-                            </Menu.Item>
+              <Menu.Item
+                key="3"
+                icon={<ShoppingCartOutlined />}
+                className="mb-20"
+              >
+                <Link href="/shop">Shop</Link>
+              </Menu.Item>
 
-                            <Menu.Item key="2" icon={<GiMagnifyingGlass />} className="mb-20">
-                                <Link href="/lookup">Lookup</Link>
-                            </Menu.Item>
+              <Menu.Item key="4" icon={<ProfileOutlined />} className="mb-20">
+                <Link href="/pages">Pages</Link>
+              </Menu.Item>
 
-                            <Menu.Item key="3" icon={<RiFileCopy2Line />} className="mb-15">
-                                <Link href="/files">Files</Link>
-                            </Menu.Item>
+              <Menu.Item key="6" icon={<SnippetsOutlined />} className="mb-20">
+                <Link href="/blog">Blog</Link>
+              </Menu.Item>
+            </Menu>
+          </div>
 
-                            <SubMenu
-                                key="4"
-                                icon={<BsFillFileEarmarkTextFill />}
-                                title="Logs"
-                                className="mb-15"
-                            >
-                                <Menu.Item key="42" icon={<RiFileTextLine />}>
-                                    <Link href="/logs/lookup">Single/Bulk Lookup</Link>
-                                </Menu.Item>
-                                <Menu.Item key="43" icon={<IoMdPie />}>
-                                    <Link href="/logs/daily-usages">Daily Usages</Link>
-                                </Menu.Item>
-                            </SubMenu>
+          <div className="flex ml-20" style={{ justifyContent: "center" }}>
+            <div className="mr-35">
+              <GrFacebook
+                className="cursor"
+                style={{ color: "white", fontSize: "24px" }}
+                onClick={() => {
+                  message.warn("Coming soon");
+                }}
+              />
+            </div>
 
-                            <Menu.Item key="5" icon={<MdShoppingBag />} className="mb-20">
-                                <Link href="/buy-credits">Buy Credits</Link>
-                            </Menu.Item>
+            <div className="mr-35">
+              <BsInstagram
+                style={{ color: "white", fontSize: "24px" }}
+                className="cursor"
+                onClick={() => {
+                  message.warn("Coming soon");
+                }}
+              />
+            </div>
 
-                            <Menu.Item key="6" icon={<GiGooeyEyedSun />} className="mb-20">
-                                <Link href="/integrations">Integrations</Link>
-                            </Menu.Item>
-
-                            <Menu.Item key="7" icon={<FaUserCircle />} className="mb-20">
-                                <Link href="/my-profile">My Profile</Link>
-                            </Menu.Item>
-                        </Menu>
-                    </div>
-
-                </div>
-            </Drawer >
+            <div className="">
+              <FaPinterestSquare
+                style={{ color: "white", fontSize: "24px" }}
+                className="cursor"
+                onClick={() => {
+                  message.warn("Coming soon");
+                }}
+              />
+            </div>
+          </div>
         </div>
-    );
+      </Drawer>
+    </div>
+  );
 };
 
 export default index;
--- Components/Navbar/index.tsx
+++ Components/Navbar/index.tsx
@@ -1,170 +1,227 @@
-import { Drawer } from 'antd';
+import MobileDrawer from "@Components/MobileDrawer";
+import { Drawer, message } from "antd";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
 
 // CSS
 require("./index.less");
 
-
 const index = (props: any) => {
+  let myNav: any;
 
-    let myNav: any;
+  const [visible, setVisible] = useState(false);
+  const [logo, setLogo] = useState(false);
 
-    const [visible, setVisible] = useState(false);
-    const [logo, setLogo] = useState(false);
+  useEffect(() => {
+    myNav = document.getElementById("header-menu-container3");
+    handleScroll();
+    window.addEventListener("scroll", handleScroll, true);
+  }, []);
 
-    useEffect(() => {
-        myNav = document.getElementById('header-menu-container3');
-        handleScroll()
-        window.addEventListener('scroll', handleScroll, true);
-    }, []);
-
-    //Changing the class while scrolling
-    let handleScroll = () => {
-        // @ts-ignore
-        if (props.default === undefined || props.default) {
-
-            if (window.pageYOffset >= 100) {
-                setLogo(true);
-                myNav.classList.remove("nav-without-colored3");
-                myNav.classList.add("nav-colored3");
-            }
-            else {
-                setLogo(false);
-                myNav.classList.add("nav-without-colored3");
-                myNav.classList.remove("nav-colored3");
-            }
-        } else {
-            setLogo(true);
-            myNav.classList.add("nav-colored3");
-            myNav.classList.remove("nav-without-colored3");
-        }
+  //Changing the class while scrolling
+  let handleScroll = () => {
+    // @ts-ignore
+    if (props.default === undefined || props.default) {
+      if (window.pageYOffset >= 100) {
+        setLogo(true);
+        myNav.classList.remove("nav-without-colored3");
+        myNav.classList.add("nav-colored3");
+      } else {
+        setLogo(false);
+        myNav.classList.add("nav-without-colored3");
+        myNav.classList.remove("nav-colored3");
+      }
+    } else {
+      setLogo(true);
+      myNav.classList.add("nav-colored3");
+      myNav.classList.remove("nav-without-colored3");
     }
+  };
 
-
-
-    return (
-        <>
-            <div id="header-menu-container3" className="header-menu-container3 full-width">
-                <div className="desktop-menu header-menu flex space-between full-width">
-                    <div className="left-menu flex flex-center-v">
-                        <div className="header-menu-items flex flex-center-v">
-
-                            <div className="flex flex-center-v imLogo">
-                                <div className="menu-logo">
-                                    <a href="/">
-                                        <img
-                                            className="cursor margin-left"
-                                            alt="landing-logo"
-                                            src={"/images/impromekLogo2.svg"}>
-                                        </img>
-                                    </a>
-                                </div>
-                            </div>
-
-                            <div className="">
-                                <Link href="/" >
-                                    <a href="/"><div className="header-menu-item3 helvetica cursor">Home</div></a>
-                                </Link>
-                            </div>
-
-                            <div className="">
-                                <Link href="/mentor-list" >
-                                    <a href="/mentor-list"><div className="header-menu-item3 helvetica cursor">Mentors List</div></a>
-                                </Link>
-                            </div>
-
-                            <div className="">
-                                <Link href="/how-it-works" >
-                                    <a href="/how-it-works"><div className="header-menu-item3 helvetica cursor">How It Works</div></a>
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
-
-                    {/* <div className="flex flex-center-v">
-                        <div className="menu-logo">
-                            <a href="/">
-                                <img
-                                    className="cursor margin-left"
-                                    alt="landing-logo"
-                                    src={logo ? "/images/impromekLogo2.svg" : "/images/impromekLogo.svg"}>
-                                </img>
-                            </a>
-                        </div>
-                    </div> */}
-
-                    <div className="right-menu flex flex-center-v">
-                        <div className="">
-                            <Link href="/registration" >
-                                <a href="/registration"><div className="header-menu-item3 helvetica cursor">Register</div></a>
-                            </Link>
-                        </div>
-
-                        <div className="loginBtn">
-                            <Link href="/login" >
-                                <a href="/login"><div className="header-menu-item3 helvetica cursor">Login</div></a>
-                            </Link>
-                        </div>
-
-                    </div>
+  return (
+    <>
+      <div
+        id="header-menu-container3"
+        className="header-menu-container3 full-width"
+      >
+        <div className="desktop-menu header-menu flex space-between full-width">
+          <div className="left-menu flex flex-center-v">
+            <div className="header-menu-items flex flex-center-v">
+              <div className="flex flex-center-v imLogo">
+                <div className="menu-logo">
+                  <Link href="/">
+                    <a href="/">
+                      <div
+                        className="navbarTitle"
+                        style={{ marginLeft: "70px" }}
+                      >
+                        Funshion
+                      </div>
+                    </a>
+                  </Link>
                 </div>
+              </div>
+            </div>
+          </div>
 
-                <div className="mobile-menu" style={{ width: '100%' }}>
-                    {/* <img alt="ham-burger" onClick={() => { setVisible(true) }} src={logo ? "/landing/hamwhite.svg" : "/images/ham.svg"}></img> */}
-                    <img alt="ham-burger" onClick={() => { setVisible(true) }} src={logo ? "/landing/hamwhite.svg" : "/landing/hamwhite.svg"}></img>
-                    <div style={{ width: '100%' }} className="center">
-                        <div className="menu-logo">
-                            <a href="/">
-                                <img
-                                    className="cursor"
-                                    alt="impromekLogo" src={logo ? "/images/impromekLogo2.svg" : "/images/impromekLogo2.svg"}></img>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+          <div className="right-menu flex flex-center-v">
+            <div className="">
+              <Link href="/">
+                <a href="/">
+                  <div className="header-menu-item3 helvetica cursor">Home</div>
+                </a>
+              </Link>
             </div>
 
-            <Drawer
-                title="Menu"
-                placement="left"
-                width={"100%"}
-                closable={true}
-                onClose={() => { setVisible(false) }}
-                visible={visible}
-            >
-                <div className="listBody">
-                    <Link href="/" >
-                        <a href="/"><div className="">Home</div></a>
-                    </Link>
-                </div>
+            <div className="">
+              <Link href="/about">
+                <a href="/about">
+                  <div className="header-menu-item3 helvetica cursor">
+                    About
+                  </div>
+                </a>
+              </Link>
+            </div>
 
-                <div className="listBody">
-                    <Link href="/mentor-list" >
-                        <a href="/mentor-list"><div className="">Mentors List</div></a>
-                    </Link>
-                </div>
+            <div className="">
+              <Link href="/shop">
+                <a href="/shop">
+                  <div className="header-menu-item3 helvetica cursor">Shop</div>
+                </a>
+              </Link>
+            </div>
 
-                <div className="listBody">
-                    <Link href="/how-it-works" >
-                        <a href="/how-it-works"><div className="">How It Works</div></a>
-                    </Link>
-                </div>
+            <div className="">
+              <Link href="/pages">
+                <a href="/pages">
+                  <div className="header-menu-item3 helvetica cursor">
+                    Pages
+                  </div>
+                </a>
+              </Link>
+            </div>
 
-                <div className="listBody">
-                    <Link href="/login" >
-                        <a href="/login"><div className="">Login</div></a>
-                    </Link>
-                </div>
+            <div className="">
+              <Link href="/blog">
+                <a href="/blog">
+                  <div className="header-menu-item3 helvetica cursor">Blog</div>
+                </a>
+              </Link>
+            </div>
 
-                <div className="listBody">
-                    <Link href="/registration" >
-                        <a href="/registration"><div className="">Register</div></a>
-                    </Link>
-                </div>
-            </Drawer >
-        </>
-    );
+            <div className="flex ml-50">
+              <div className="mr-35">
+                <img
+                  src="/images/magnify.svg"
+                  alt=""
+                  className="cursor"
+                  onClick={() => {
+                    message.warn("Coming soon");
+                  }}
+                />
+              </div>
+
+              <div className="mr-35">
+                <img
+                  src="/images/avatar.svg"
+                  alt=""
+                  className="cursor"
+                  onClick={() => {
+                    message.warn("Coming soon");
+                  }}
+                />
+              </div>
+
+              <div className="">
+                <img
+                  src="/images/brifcase.svg"
+                  alt=""
+                  className="cursor"
+                  onClick={() => {
+                    message.warn("Coming soon");
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* <div className="mobile-menu" style={{ width: "100%" }}><MobileDrawer></MobileDrawer></div> */}
+
+        <div className="mobile-menu" style={{ width: "100%" }}>
+          <div>
+            <AiOutlineMenu
+              style={{ fontSize: "24px", fontWeight: 500 }}
+              className=""
+              onClick={() => {
+                setVisible(true);
+              }}
+            />
+          </div>
+          <div className="m-auto">
+            <Link href="/">
+              <a href="/">
+                <div className="navbarTitle">Funshion</div>
+              </a>
+            </Link>
+          </div>
+        </div>
+        <MobileDrawer visible={visible} setVisible={setVisible}></MobileDrawer>
+      </div>
+
+      {/* <Drawer
+        title="Menu"
+        placement="left"
+        width={"100%"}
+        closable={true}
+        onClose={() => {
+          setVisible(false);
+        }}
+        visible={visible}
+      >
+        <div className="listBody">
+          <Link href="/">
+            <a href="/">
+              <div className="">Home</div>
+            </a>
+          </Link>
+        </div>
+
+        <div className="listBody">
+          <Link href="/mentor-list">
+            <a href="/mentor-list">
+              <div className="">Mentors List</div>
+            </a>
+          </Link>
+        </div>
+
+        <div className="listBody">
+          <Link href="/how-it-works">
+            <a href="/how-it-works">
+              <div className="">How It Works</div>
+            </a>
+          </Link>
+        </div>
+
+        <div className="listBody">
+          <Link href="/login">
+            <a href="/login">
+              <div className="">Login</div>
+            </a>
+          </Link>
+        </div>
+
+        <div className="listBody">
+          <Link href="/registration">
+            <a href="/registration">
+              <div className="">Register</div>
+            </a>
+          </Link>
+        </div>
+      </Drawer> */}
+    </>
+  );
 };
 
 export default index;
