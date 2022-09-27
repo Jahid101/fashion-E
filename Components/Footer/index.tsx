5769d02359932cad0884c4ed0ba2ba4a39fdaa6c
Robustdevs5 <jahidhasananik.official@gmail.com>
2022-09-27 13:00:32 +0600

    footer

--- Components/Footer/index.tsx
+++ Components/Footer/index.tsx
@@ -1,70 +1,51 @@
+import { Col, Row } from "antd";
+import Link from "next/link";
 import React from "react";
-import {
-  Box,
-  Container,
-  Row,
-  Column,
-  FooterLink,
-  Heading,
-} from "./FooterStyles";
-  
-const Footer = () => {
+require("./index.less");
+
+const index = () => {
   return (
-    <Box>
-      <Container>
-        <Row>
-          <Column>
-            <Heading>Funshion</Heading>
-            <FooterLink href="#">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</FooterLink>
-          </Column>
-          <Column>
-            <Heading>Need Help?</Heading>
-            <FooterLink href="#">Writing</FooterLink>
-            <FooterLink href="#">Internships</FooterLink>
-            <FooterLink href="#">Coding</FooterLink>
-            <FooterLink href="#">Teaching</FooterLink>
-          </Column>
-          <Column>
-            <Heading>Need Help?</Heading>
-            <FooterLink href="#">Uttar Pradesh</FooterLink>
-            <FooterLink href="#">Ahemdabad</FooterLink>
-            <FooterLink href="#">Indore</FooterLink>
-            <FooterLink href="#">Mumbai</FooterLink>
-          </Column>
-          <Column>
-            <Heading>Need Help?</Heading>
-            <FooterLink href="#">
-              <i className="fab fa-facebook-f">
-                <span style={{ marginLeft: "10px" }}>
-                  Facebook
-                </span>
-              </i>
-            </FooterLink>
-            <FooterLink href="#">
-              <i className="fab fa-instagram">
-                <span style={{ marginLeft: "10px" }}>
-                  Instagram
-                </span>
-              </i>
-            </FooterLink>
-            <FooterLink href="#">
-              <i className="fab fa-twitter">
-                <span style={{ marginLeft: "10px" }}>
-                  Twitter
-                </span>
-              </i>
-            </FooterLink>
-            <FooterLink href="#">
-              <i className="fab fa-youtube">
-                <span style={{ marginLeft: "10px" }}>
-                  Youtube
-                </span>
-              </i>
-            </FooterLink>
-          </Column>
-        </Row>
-      </Container>
-    </Box>
+    <div className="footer">
+      <Row gutter={[50, 30]}>
+        <Col xxl={6} xl={6} lg={12} md={12} sm={24} xs={24}>
+          <Link href="/">
+            <a href="/">
+              <div className="footerTitle">Funshion</div>
+            </a>
+          </Link>
+
+          <div className="footerSubTitle">
+            It is a long established fact that a reader will be distracted by
+            the readable content of a page when looking at its layout.
+          </div>
+        </Col>
+
+        <Col xxl={6} xl={6} lg={12} md={12} sm={24} xs={24}>
+          <div className="footerTitle">Need Help?</div>
+          <div className="footerSubTitle">Support</div>
+          <div className="footerSubTitle">Get Started</div>
+          <div className="footerSubTitle">Terms of like</div>
+          <div className="footerSubTitle">Privacy Policy</div>
+        </Col>
+
+        <Col xxl={6} xl={6} lg={12} md={12} sm={24} xs={24}>
+          <div className="footerTitle">Need Help?</div>
+          <div className="footerSubTitle">Support</div>
+          <div className="footerSubTitle">Get Started</div>
+          <div className="footerSubTitle">Terms of like</div>
+          <div className="footerSubTitle">Privacy Policy</div>
+        </Col>
+
+        <Col xxl={6} xl={6} lg={12} md={12} sm={24} xs={24}>
+          <div className="footerTitle">Need Help?</div>
+          <div className="footerSubTitle">Support</div>
+          <div className="footerSubTitle">Get Started</div>
+          <div className="footerSubTitle">Terms of like</div>
+          <div className="footerSubTitle">Privacy Policy</div>
+        </Col>
+      </Row>
+    </div>
   );
 };
-export default Footer;
+
+export default index;
